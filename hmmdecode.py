19d30e23938fb6cb2bf4133738644a9ac57e7011
Pankaj Dabade <dabade@usc.edu>
2016-04-10 18:32:07 -0700

    Added BluePrint of the Viterbi Algorithm
    Dummy code for Viterbi Algorithm.
    Missing: Recursive Generator function to compute list of probabilities
    Recursive Generator function to compute backpointers
    
    Signed-off-by: Pankaj Dabade <dabade@usc.edu>

--- hmmdecode.py
+++ hmmdecode.py
@@ -6,6 +6,8 @@ import sys
 
 fname  = sys.argv[1]
 output = 'hmmoutput.txt'
+P      = dict()
+B      = dict()
 # Getting the learned model from the output file. hmmmodel.txt
 #============================================================================== 
 with open('hmmmodel.txt') as f:
@@ -14,17 +16,45 @@ freqTags   = data['TAG']
 freqTrans  = data['TRANSITION']
 Emission   = data['Emission']
 Transition = data['Transition']
+
 #============================================================================== 
 def viterbi(line):
     line = line.split()
+    T    = len(line)
+    #==========================================================================e
+    # Initialization step at t = 1
     for tag in freqTags:
+        P[tag] = dict()
+        B[tag] = dict()
+        B[tag][1] = 'start'
         try:
-            print float(Transition['start'][tag])/freqTrans['start'], 'start', tag 
+            P[tag][1] = float(Transition['start'][tag])/freqTrans['start']
         except KeyError:
-            print 0, 'start', tag
+            P[tag][1] = 0
+    #==========================================================================
+    # Recursion step for the remaining time points
+    for t in range(T):
+        print line[t]
+        # tag = q(current state)
+        for tag in freqTags:
+            #print t + 2
+            P[tag][t+2] = max(1, 2)
+            B[tag][t+2] = max(1, 2)
+    #==========================================================================
+    # Termination Step
+    # Some code to follow up here
+
 #============================================================================== 
 #Reading files from the test data one line at a time
 fhand = open(fname, 'r')
 for line in fhand:
     line = line.strip()
     viterbi(line)
+
+'''
+for tag in P:
+    print tag , P[tag]
+for tag in B:
+    print tag , B[tag]
+'''
+#==============================================================================
