3f5589d4b893ef94689c9bd325f177e00058f174
Pankaj Dabade <dabade@usc.edu>
2016-04-12 06:09:40 -0700

    Viterbi Algorithm complete implementation
    I hate the lack of parenthesis. Copy Paste is hard.
    I spent 3 hours debugging indentation led semantic error.
    Signed-off-by: Pankaj Dabade <dabade@usc.edu>

--- hmmdecode.py
+++ hmmdecode.py
@@ -16,35 +16,19 @@ Emission   = data['Emission']
 Transition = data['Transition']
 
 #============================================================================== 
-def tracePath(B):
-    pass
-#==============================================================================
-def computeProbability(P, t, tag, word):
-    maximum = None
-    pointer = None
-    try:
-        b   = Emission[word][tag]
-    except KeyError:
-        b   = 1
-    #==========================================================================e
-    for var in Transition:
-        if var == 'start': continue
-        if P[var][t] == 0:
-            temp = 0
-        else:
-            temp = b * P[var][t] * Transition[var][tag]
-
-        if not maximum or maximum < temp:
-            maximum = temp
-            pointer = var
-    #==========================================================================e
-    return maximum, pointer
+def tracePath(s, line, T, B):
+    for t in range(T, 0, -1):
+        line[t - 1] =  line[t - 1] + '/' + s
+        s = B[s][t]
+    line = ' '.join(line)
+    return line
 
 #==============================================================================
 def viterbi(line):
     if not line: return
     line = line.split()
     T    = len(line)
+    length = T
     P    = dict()
     B    = dict()
     #==========================================================================e
@@ -59,29 +43,86 @@ def viterbi(line):
         except KeyError:
             # KeyError is for absense of OBSERVATION for current tag
             P[tag][1] = 0
+        if P[tag][1]:
+            print tag, P[tag][1]
     #==========================================================================
     # Recursion step for the remaining time points
     for t in range(1, T):
+        word = line[t]
         for tag in Transition:
             if tag == 'start': continue
-            # If the previous tag was zero, its a dead path
-            if P[tag][t] == 0:
-                P[tag][t+1] = 0
+            #==================================================================
+            P[tag][t+1] = 0
+            # Unknown Word. Ignore Observation.(This should be in outside loop)
+            if not word in Emission:
+                b = 1
             else:
-                P[tag][t+1], B[tag][t+1] = computeProbability(P, t, tag, line[t])
+                # Word exists. But does it exists for current tag?
+                if tag in Emission[word]:
+                    b = Emission[word][tag]
+                else:
+                    continue
+            #==========================================================
+            for var in Transition:
+                if var == 'start': continue
+                if P[var][t] == 0:
+                    temp = 0
+                else:
+                    temp = P[var][t] * Transition[var][tag] * b
+
+                if not P[tag][t+1] or P[tag][t+1] < temp:
+                    P[tag][t+1] = temp
+                    B[tag][t+1] = var
+            #==================================================================
+    # End of outermost for loop
     #==========================================================================
     # Termination Step
-    # Some code to follow up here
-    #for key, value in P.items():
-    #    print key, value
-    return tracePath(B)
+    print 'Length:' , length
+    mps = None
+    most_probable_state = None
+    for tag in P:
+        if not mps or mps < P[tag][length]:
+            mps = P[tag][length]
+            most_probable_state = tag
+    #==========================================================================
+    '''
+    print most_probable_state, B[most_probable_state]
+    print 'AQ'
+    print 'NC'
+    print 'SP'
+    print 'NC'
+    print 'SP'
+    print 'CS'
+    print 'VM'
+    print 'PP'
+    print 'NP'
+    print 'FF'
+    print 'NC'
+    print 'DA'
+    print 'VM'
+    print 'PR'
+    print 'NP'
+    print 'CC'
+    print 'NP'
+    print 'NC'
+    print 'PX'
+    print 'DA'
+    print 'SP'
+    print 'NC'
+    print 'DI'
+    print 'SP'
+    '''
+    return tracePath(most_probable_state, line, T, B)
 
 #============================================================================== 
 #Reading files from the test data one line at a time
 fhand = open(fname, 'r')
 for line in fhand:
     line = line.strip()
-    viterbi(line)
+    #line = viterbi(line)
+    line  = viterbi(line)
+    print line
+    #print line
 fhand.close()
 
 '''
